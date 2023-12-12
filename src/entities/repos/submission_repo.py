d6c5b6bc7d5e6795d613f6f32a766885148370b4
Adam <41971533+jcadam14@users.noreply.github.com>
2023-12-11 21:13:17 -0500

    Initial push of the SubmissioDAO/DTO structure.  Adds the ability to store submission results to the database
    SBL needs to store submission results to the database for query/dashboard/metrics use

---
+++ src/entities/repos/submission_repo.py
@@ -0,0 +1,53 @@
+from typing import List
+
+from sqlalchemy import select, func, text
+from sqlalchemy.orm import joinedload
+from sqlalchemy.ext.asyncio import AsyncSession
+
+import pandas as pd
+import json
+
+from entities.models import (
+    SubmissionDAO,
+    ValidationResultDAO,
+    RecordDAO
+)
+
+async def get_submission(
+    session: AsyncSession,
+    submission_id: str
+) -> SubmissionDAO:
+    async with session.begin():
+        stmt = (
+            select(SubmissionDAO)
+            .options(joinedload(SubmissionDAO.results).joinedload(ValidationResultDAO.records))
+            .filter(SubmissionDAO.submission_id == submission_id)
+        ) 
+        return await session.scalar(stmt)
+
+async def add_submission(
+    session: AsyncSession,
+    submission_id: str,
+    submitter: str,
+    lei: str,
+    results: pd.DataFrame
+) -> SubmissionDAO:
+    async with session.begin():
+        findings_by_v_id_df = results.reset_index().set_index(['validation_id'])
+        submission = SubmissionDAO(submission_id=submission_id, submitter=submitter, lei=lei)
+        validation_results = []
+        for v_id_idx, v_id_df in findings_by_v_id_df.groupby(by='validation_id'):
+            v_head = v_id_df.iloc[0]
+            print(f"Building results for error code {v_id_idx}")
+            result = ValidationResultDAO(validation_id=v_id_idx, field_name=v_head.at['field_name'], severity=v_head.at['validation_severity'])
+            records = []
+            for rec_no, rec_df in v_id_df.iterrows():
+                print(f'{rec_no} Rec Def: {rec_df}')
+                record = RecordDAO(record=rec_df.at['record_no'], data=rec_df.at['field_value'])
+                records.append(record)
+            result.records = records
+            validation_results.append(result)
+        submission.results = validation_results
+        session.add(submission)
+
+        return submission
---
+++ tests/entities/conftest.py
@@ -0,0 +1,61 @@
+import asyncio
+import pytest
+
+from asyncio import current_task
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    AsyncEngine,
+    async_scoped_session,
+    async_sessionmaker,
+)
+from entities.models import Base
+
+
+@pytest.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    try:
+        yield loop
+    finally:
+        loop.close()
+
+
+@pytest.fixture(scope="session")
+def engine():
+    return create_async_engine("sqlite+aiosqlite://")
+
+
+@pytest.fixture(scope="function", autouse=True)
+async def setup_db(
+    request: pytest.FixtureRequest,
+    engine: AsyncEngine,
+    event_loop: asyncio.AbstractEventLoop,
+):
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+        
+    def teardown():
+        async def td():
+            async with engine.begin() as conn:
+                await conn.run_sync(Base.metadata.drop_all)
+
+        event_loop.run_until_complete(td())
+
+    request.addfinalizer(teardown)
+
+
+@pytest.fixture(scope="function")
+async def transaction_session(session_generator: async_scoped_session):
+    async with session_generator() as session:
+        yield session
+
+
+@pytest.fixture(scope="function")
+async def query_session(session_generator: async_scoped_session):
+    async with session_generator() as session:
+        yield session
+
+
+@pytest.fixture(scope="function")
+def session_generator(engine: AsyncEngine):
+    return async_scoped_session(async_sessionmaker(engine, expire_on_commit=False), current_task)
