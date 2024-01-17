f1a7c743e3e714852a2bfb41e24cc783e096033c
jcadam14 <41971533+jcadam14@users.noreply.github.com>
2024-01-17 13:47:10 -0700

    Cleaned up branch to no longer be a full merge of #7 and #18 (#20)
    Closes #13 
    
    Created a new branch that focuses purely on the alembic script needs
    instead of a full merge of #7 and #18 into the #13 branch. Makes it so
    only the code related to the migrations is needed for review. Originally
    I merged them all together to test starting the service in poetry to
    make sure the tables are created correctly in Postgres but that made the
    PR have a lot of files that were already covered in other PRs. Still the
    case with the entities/models directory but couldn't get around that.
    
    ## Additions
    
    - Added db_revision folder using alembic init and updated files for SBL
    specifics (using user-fi as an example)
    - Added migration files using alembic revision for each submission table
    (submission, filing, filing_period)
    - Updated migration files for table specifics
    - Added pytest migrations tests for new tables, columns, and fks
    - Had to merge in the entities/models dir since the migrations use the
    enumerations
    
    ## Removals
    
    -
    
    ## Changes
    
    -
    
    ## Testing
    
    1. Run pytest in poetry shell (or poetry run pytest) and verify the
    tests pass
    
    ## Screenshots
    
    
    ## Notes
    
    -
    
    ## Todos
    
    -
    
    ## Checklist
    
    - [ ] PR has an informative and human-readable title
    - [ ] Changes are limited to a single goal (no scope creep)
    - [ ] Code can be automatically merged (no conflicts)
    - [ ] Code follows the standards laid out in the [development
    playbook](https://github.com/cfpb/development)
    - [ ] Passes all existing automated tests
    - [ ] Any _change_ in functionality is tested
    - [ ] New functions are documented (with a description, list of inputs,
    and expected output)
    - [ ] Placeholder code is flagged / future todos are captured in
    comments
    - [ ] Visually tested in supported browsers and devices (see checklist
    below :point_down:)
    - [ ] Project documentation has been updated (including the "Unreleased"
    section of the CHANGELOG)
    - [ ] Reviewers requested with the [Reviewers
    tool](https://help.github.com/articles/requesting-a-pull-request-review/)
    :arrow_right:
    
    ## Testing checklist
    
    ### Browsers
    
    - [ ] Chrome
    - [ ] Firefox
    - [ ] Safari
    - [ ] Internet Explorer 8, 9, 10, and 11
    - [ ] Edge
    - [ ] iOS Safari
    - [ ] Chrome for Android
    
    ### Accessibility
    
    - [ ] Keyboard friendly
    - [ ] Screen reader friendly
    
    ### Other
    
    - [ ] Is useable without CSS
    - [ ] Is useable without JS
    - [ ] Flexible from small to large screens
    - [ ] No linting errors or warnings
    - [ ] JavaScript tests are passing

---
+++ src/entities/models/dto.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from typing import Dict, Any
+from pydantic import BaseModel, ConfigDict
+from .model_enums import FilingType, FilingState, SubmissionState
+
+
+class SubmissionDTO(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int | None = None
+    submitter: str
+    state: SubmissionState | None = None
+    validation_ruleset_version: str | None = None
+    validation_json: Dict[str, Any] | None = None
+    filing: int
+
+
+class FilingDTO(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int | None = None
+    lei: str
+    state: FilingState
+    filing_period: int
+    institution_snapshot_id: str
+
+
+class FilingPeriodDTO(BaseModel):
+    model_config = ConfigDict(from_attributes=True)
+
+    id: int | None = None
+    name: str
+    start_period: datetime
+    end_period: datetime
+    due: datetime
+    filing_type: FilingType
