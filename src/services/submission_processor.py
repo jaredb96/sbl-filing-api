fc42dd76ad297a18cfca893fc6e63ff98415a09a
jmcguffee <justin.mcguffee@cfpb.gov>
2024-01-30 14:46:29 -0500

    Update table to correct state as validation progresses

--- src/services/submission_processor.py
+++ src/services/submission_processor.py
@@ -1,6 +1,11 @@
 from io import BytesIO
+
 from regtech_data_validator.create_schemas import validate_phases
 import pandas as pd
+import importlib.metadata as imeta
+
+from src.entities.models import SubmissionDAO, SubmissionState
+from src.entities.repos.submission_repo import update_submission
 
 
 async def upload_to_storage(lei: str, submission_id: str, content: bytes):
@@ -10,6 +15,37 @@ async def upload_to_storage(lei: str, submission_id: str, content: bytes):
 
 async def validate_submission(lei: str, submission_id: str, content: bytes):
     df = pd.read_csv(BytesIO(content), dtype=str, na_filter=False)
+    validator_version = imeta.version('regtech-data-validator')
+
+    # Set VALIDATION_IN_PROGRESS
+    await update_submission(
+        SubmissionDAO(
+            submitter=submission_id,
+            state=SubmissionState.VALIDATION_IN_PROGRESS
+        )
+    )
+
+    # Validate phases
     result = validate_phases(df, {"lei": lei})
-    # Persist resulting dataframe to tables
-    pass
+
+    # Update tables with response
+    if not result[0]:
+        sub_state = SubmissionState.VALIDATION_WITH_ERRORS if 'error' in df['validation_severity'].values else SubmissionState.VALIDATION_WITH_WARNINGS
+        await update_submission(
+            SubmissionDAO(
+                submitter=submission_id,
+                state=sub_state,
+                validation_ruleset_version=validator_version,
+                validation_json=result[1].to_json
+            )
+        )
+    else:
+        await update_submission(
+            SubmissionDAO(
+                submitter=submission_id,
+                state=SubmissionState.VALIDATION_SUCCESSFUL,
+                validation_ruleset_version=validator_version,
+                validation_json=result[1].to_json
+            )
+        )
+        
