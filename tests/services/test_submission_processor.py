30cd0b78422dab04900f73527c048fd966bbff5b
lchen-2101 <73617864+lchen-2101@users.noreply.github.com>
2024-02-27 16:29:02 -0500

    Fix/75 no mkdir when s3 (#76)
    closes #75

--- tests/services/test_submission_processor.py
+++ tests/services/test_submission_processor.py
@@ -2,6 +2,7 @@ from fastapi import HTTPException
 import pytest
 from unittest.mock import Mock, ANY
 from pytest_mock import MockerFixture
+from config import FsProtocol, settings
 from services.submission_processor import upload_to_storage
 
 
@@ -20,12 +21,25 @@ def mock_fs_func(mocker: MockerFixture, mock_fs: Mock) -> Mock:
 
 async def test_upload(mocker: MockerFixture, mock_fs_func: Mock, mock_fs: Mock):
     with mocker.mock_open(mock_fs.open):
-        await upload_to_storage("test", "test", b"test content")
+        await upload_to_storage("test", "test", b"test content local")
     mock_fs_func.assert_called()
     mock_fs.mkdirs.assert_called()
     mock_fs.open.assert_called_with(ANY, "wb")
     file_handle = mock_fs.open()
-    file_handle.write.assert_called_with(b"test content")
+    file_handle.write.assert_called_with(b"test content local")
+
+
+async def test_upload_s3_no_mkdir(mocker: MockerFixture, mock_fs_func: Mock, mock_fs: Mock):
+    default_fs_proto = settings.upload_fs_protocol
+    settings.upload_fs_protocol = FsProtocol.S3
+    with mocker.mock_open(mock_fs.open):
+        await upload_to_storage("test", "test", b"test content s3")
+    mock_fs_func.assert_called()
+    mock_fs.mkdirs.assert_not_called()
+    mock_fs.open.assert_called_with(ANY, "wb")
+    file_handle = mock_fs.open()
+    file_handle.write.assert_called_with(b"test content s3")
+    settings.upload_fs_protocol = default_fs_proto
 
 
 async def test_upload_failure(mocker: MockerFixture, mock_fs_func: Mock, mock_fs: Mock):
