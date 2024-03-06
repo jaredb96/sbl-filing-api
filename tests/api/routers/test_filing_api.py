65cb7b019f0ee7ab00935182d6fd0abdf903835c
jmcguffee <justin.mcguffee@cfpb.gov>
2024-03-06 11:06:00 -0500

    Removed unnecessary lines

--- tests/api/routers/test_filing_api.py
+++ tests/api/routers/test_filing_api.py
@@ -135,10 +135,6 @@ class TestFilingApi:
         assert res.status_code == 202
 
     def test_unauthed_upload_file(self, mocker: MockerFixture, app_fixture: FastAPI, submission_csv: str):
-        mock_upload = mocker.patch("services.submission_processor.upload_to_storage")
-        mock_upload.return_value = None
-        mock_validate_submission = mocker.patch("services.submission_processor.validate_submission")
-        mock_validate_submission.return_value = None
         files = {"file": ("submission.csv", open(submission_csv, "rb"))}
         client = TestClient(app_fixture)
         res = client.post("/v1/filing/123456790/submissions/1", files=files)
