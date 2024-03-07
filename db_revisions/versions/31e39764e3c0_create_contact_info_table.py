1389d0bc96b425dfb3fb3e9d681a4857ee369165
Nargis Sultani <nargis.sultani@cfpb.gov>
2024-03-07 11:39:26 -0500

    modified alembic two heads error

--- db_revisions/versions/31e39764e3c0_create_contact_info_table.py
+++ db_revisions/versions/31e39764e3c0_create_contact_info_table.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "31e39764e3c0"
-down_revision: Union[str, None] = "19fccbf914bc"
+down_revision: Union[str, None] = "8e8faceb46bd"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
