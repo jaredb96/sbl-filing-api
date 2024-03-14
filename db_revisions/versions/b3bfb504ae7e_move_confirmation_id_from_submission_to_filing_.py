4ca2da829e9279fa893ef7a34d040a32ca525d9d
Nargis Sultani <nargis.sultani@cfpb.gov>
2024-03-14 14:54:35 -0400

    fixed alembic revision two head error

--- db_revisions/versions/b3bfb504ae7e_move_confirmation_id_from_submission_to_filing_.py
+++ db_revisions/versions/b3bfb504ae7e_move_confirmation_id_from_submission_to_filing_.py
@@ -1,7 +1,7 @@
 """move confirmation id from submission to filing table
 
 Revision ID: b3bfb504ae7e
-Revises: 8eaef8ce4c23
+Revises: b70d06f93029
 Create Date: 2024-03-14 03:18:39.063892
 
 """
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "b3bfb504ae7e"
-down_revision: Union[str, None] = "8eaef8ce4c23"
+down_revision: Union[str, None] = "b70d06f93029"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
