fa114a2692f306fc4b9111c0fe3d8b23be9eaa11
nargis-sultani <102271080+nargis-sultani@users.noreply.github.com>
2024-03-14 10:43:06 -0400

    changed post to put for contact-info endpoint (#109)
    [Short description explaining the high-level reason for the pull
    request]
    
    ## Additions
    
    -
    
    ## Removals
    
    -
    
    ## Changes
    
    -
    
    ## Testing
    
    1.
    
    ## Screenshots
    
    
    ## Notes
    
    -
    
    ## Todos
    
    -
    
    ## Checklist
    
    - [ ] PR has an informative and human-readable title
    - [ ] Changes are limited to a single goal (no scope creep)
    - [ ] Code can be automatically merged (no conflicts)
    - [ ] Code follows the standards laid out in the [development
    playbook](https://github.com/cfpb/development)
    - [ ] Passes all existing automated tests
    - [ ] Any _change_ in functionality is tested
    - [ ] New functions are documented (with a description, list of inputs,
    and expected output)
    - [ ] Placeholder code is flagged / future todos are captured in
    comments
    - [ ] Visually tested in supported browsers and devices (see checklist
    below :point_down:)
    - [ ] Project documentation has been updated (including the "Unreleased"
    section of the CHANGELOG)
    - [ ] Reviewers requested with the [Reviewers
    tool](https://help.github.com/articles/requesting-a-pull-request-review/)
    :arrow_right:
    
    ## Testing checklist
    
    ### Browsers
    
    - [ ] Chrome
    - [ ] Firefox
    - [ ] Safari
    - [ ] Internet Explorer 8, 9, 10, and 11
    - [ ] Edge
    - [ ] iOS Safari
    - [ ] Chrome for Android
    
    ### Accessibility
    
    - [ ] Keyboard friendly
    - [ ] Screen reader friendly
    
    ### Other
    
    - [ ] Is useable without CSS
    - [ ] Is useable without JS
    - [ ] Flexible from small to large screens
    - [ ] No linting errors or warnings
    - [ ] JavaScript tests are passing
    
    ---------
    
    Co-authored-by: Nargis Sultani <nargis.sultani@cfpb.gov>

--- tests/api/routers/test_filing_api.py
+++ tests/api/routers/test_filing_api.py
@@ -350,7 +350,7 @@ class TestFilingApi:
         res = client.get("/v1/filing/institutions/1234567890/filings/2024/contact-info")
         assert res.status_code == 204
 
-    async def test_unauthed_post_contact_info(self, mocker: MockerFixture, app_fixture: FastAPI, unauthed_user_mock):
+    async def test_unauthed_put_contact_info(self, mocker: MockerFixture, app_fixture: FastAPI, unauthed_user_mock):
         contact_info_json = {
             "id": 1,
             "filing": 1,
@@ -365,10 +365,10 @@ class TestFilingApi:
             "email": "name_1@email.test",
         }
         client = TestClient(app_fixture)
-        res = client.post("/v1/filing/institutions/1234567890/filings/2024/contact-info", json=contact_info_json)
+        res = client.put("/v1/filing/institutions/1234567890/filings/2024/contact-info", json=contact_info_json)
         assert res.status_code == 403
 
-    def test_post_contact_info(self, mocker: MockerFixture, app_fixture: FastAPI, authed_user_mock: Mock):
+    def test_put_contact_info(self, mocker: MockerFixture, app_fixture: FastAPI, authed_user_mock: Mock):
         mock = mocker.patch("entities.repos.submission_repo.update_contact_info")
         mock.return_value = ContactInfoDAO(
             id=1,
@@ -397,7 +397,7 @@ class TestFilingApi:
             "phone": "112-345-6789",
             "email": "name_1@email.test",
         }
-        res = client.post("/v1/filing/institutions/1234567890/filings/2024/contact-info", json=contact_info_json)
+        res = client.put("/v1/filing/institutions/1234567890/filings/2024/contact-info", json=contact_info_json)
 
         assert res.status_code == 200
         mock.assert_called_with(
