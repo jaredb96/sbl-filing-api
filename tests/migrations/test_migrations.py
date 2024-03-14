de36072811becba813e7c3e64c6a3e88b92a2bb5
Nargis Sultani <nargis.sultani@cfpb.gov>
2024-03-14 10:42:49 -0400

    updated test

--- tests/migrations/test_migrations.py
+++ tests/migrations/test_migrations.py
@@ -178,7 +178,3 @@ def test_migration_to_8eaef8ce4c23(alembic_runner: MigrationContext, alembic_eng
 
 def test_migration_to_b70d06f93029(alembic_runner: MigrationContext, alembic_engine: Engine):
     alembic_runner.migrate_up_to("b70d06f93029")
-
-    inspector = sqlalchemy.inspect(alembic_engine)
-
-    assert "state" in [c["name"] for c in inspector.get_columns("submission")]
